3b61596e84f567332dc07c455e76ac25ab7c99b0
Vitalii Perminov <gruver2007@ya.ru>
2022-07-06 22:28:19 +0500

    config bucket for tf

--- tf/versions.tf
+++ tf/versions.tf
@@ -5,4 +5,14 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "terraform-object-storage-pandamys"
+    region     = "ru-central1"
+    key        = "state/pandamys.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
